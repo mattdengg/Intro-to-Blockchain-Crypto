12b28536b33afb6d81cd49ff3f8601c6c4040be4
Matthew Deng <75967945+mattdengg@users.noreply.github.com>
2025-10-16 01:55:36 -0400

    Update op.py

--- Bitcoin/Blockchain/Backend/core/EllepticCurve/op.py
+++ Bitcoin/Blockchain/Backend/core/EllepticCurve/op.py
@@ -1,8 +1,3 @@
-""" 
-Copyright (c) 2021 Codiesalert.com
-These scripts shouldn't be used for commercial purpose without Codies Alert Permission
-Any violations may lead to legal action
-"""
 from Blockchain.Backend.util.util import hash160
 from Blockchain.Backend.core.EllepticCurve.EllepticCurve import Sha256Point, Signature
 
