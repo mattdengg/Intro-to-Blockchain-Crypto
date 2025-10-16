5990148fce29b248412ff496b23ed3d6b9edbf3a
Matthew Deng <75967945+mattdengg@users.noreply.github.com>
2025-10-16 01:57:19 -0400

    Created the bitcoin address (#4)
    * Created the bitcoin address
    
    * Update FieldElement.py
    
    * Update EllepticCurve.py
    
    * Update op.py
    
    * Update Point.py
    
    * Update Sha256Field.py

---
+++ Bitcoin/Blockchain/Backend/core/EllepticCurve/Sha256Field.py
@@ -0,0 +1,12 @@
+from FieldElement import FieldElement
+from Point import Point
+
+P = 2 ** 256 - 2 ** 32 - 977
+
+
+class Sha256Field(FieldElement):
+    def __init__(self, num, prime=None):
+        super().__init__(num=num, prime=P)
+
+    def __repr__(self):
+        return "{:x}".format(self.num).zfill(64)
