f4866e79012617be311267c2490f9840bae8ffdf
Matthew Deng <75967945+mattdengg@users.noreply.github.com>
2025-10-25 19:45:08 -0400

    Created a coinbase transaction (#6)

--- Bitcoin/Blockchain/Backend/core/Tx.py
+++ Bitcoin/Blockchain/Backend/core/Tx.py
@@ -1,4 +1,32 @@
 from Blockchain.Backend.core.Script import Script
+from Blockchain.Backend.util.util import int_to_little_endian, bytes_needed, decode_base58, little_endian_to_int
+
+ZERO_HASH = b'\0' * 32
+REWARD = 50
+
+PRIVATE_KEY = '11006542436963211066068844580937223233186503174135729272920753217244439735835'
+MINER_ADDRESS = '1EGizaAaBHhRSqdebu2mkrqzfxdUaEKChN'
+
+class CoinbaseTx:
+    def __init__(self, BlockHeight):
+        self.BlockHeightInLittleEndian = int_to_little_endian(BlockHeight, bytes_needed(BlockHeight))
+
+    def CoinbaseTransaction(self):
+        prev_tx = ZERO_HASH
+        prev_index = 0xFFFFFFFF
+
+        tx_ins = []
+        tx_ins.append(TxIn(prev_tx, prev_index))
+        tx_ins[0].script_sig.cmds.append(self.BlockHeightInLittleEndian)
+
+        tx_outs = []
+        target_amount = REWARD * 100000000
+        target_h160 = decode_base58(MINER_ADDRESS)
+        target_script = Script.p2pkh_script(target_h160)
+        tx_outs.append(TxOut(amount = target_amount, script_pubkey= target_script))
+
+        return Tx(1, tx_ins, tx_outs, 0)
+
 
 class Tx:
     def __init__(self, version, tx_inputs, tx_outputs, locktime):
@@ -6,6 +34,52 @@ class Tx:
         self.tx_inputs = tx_inputs
         self.tx_outputs = tx_outputs
         self.locktime = locktime
+
+    def is_coinbase(self):
+        """
+        Checks that there is exactly one input
+        Grabs the first input and check if the prev_tx is b'\x00' * 32
+        Check that the first input prev_index is 0xffffffff
+        """
+        if len(self.tx_inputs) != 1:
+            return False
+
+        first_input = self.tx_inputs[0]
+
+        if first_input.prev_tx != b"\x00" * 32:
+            return False
+
+        if first_input.prev_index != 0xFFFFFFFF:
+            return False
+
+        return True
+
+    def to_dict(self):
+        """
+        Converts Coinbase Transaction
+            Converts prev_tx Hash in hex from bytes
+            Converts BlockHeight in hex which is stores in Script signature
+        """
+
+        if self.is_coinbase():
+            self.tx_inputs[0].prev_tx = self.tx_inputs[0].prev_tx.hex()
+            self.tx_inputs[0].script_sig.cmds[0] = little_endian_to_int(self.tx_inputs[0].script_sig.cmds[0])
+            self.tx_inputs[0].script_sig = self.tx_inputs[0].script_sig.__dict__
+
+        self.tx_inputs[0] = self.tx_inputs[0].__dict__
+
+        """
+        Convert Transaction Output to dic
+            If there are numbers, we don't need to do anything
+            If values is in bytes, convert to hex
+            Loop through all the TxOut objects and convert them to dict
+        """
+        self.tx_outputs[0].script_pubkey.cmds[2] = self.tx_outputs[0].script_pubkey.cmds[2].hex()
+        self.tx_outputs[0].script_pubkey = self.tx_outputs[0].script_pubkey.__dict__
+        self.tx_outputs[0] = self.tx_outputs[0].__dict__
+
+        return self.__dict__
+
 
 class TxIn:
     def __init__(self, prev_tx, prev_index, script_sig = None, seq = 0xffffffff):
--- Bitcoin/Blockchain/Backend/util/util.py
+++ Bitcoin/Blockchain/Backend/util/util.py
@@ -1,6 +1,10 @@
 import hashlib
 from Crypto.Hash import RIPEMD160
 from hashlib import sha256
+from math import log
+
+from Blockchain.Backend.core.EllepticCurve.EllepticCurve import BASE58_ALPHABET
+
 
 def hash256(s):
     """
@@ -10,3 +14,31 @@ def hash256(s):
 
 def hash160(s):
     return RIPEMD160.new(sha256(s).digest()).digest()
+
+def bytes_needed(num):
+    if num == 0 :
+        return 1
+    return int(log(num, 256)) + 1
+
+def int_to_little_endian(n, length):
+    """Int to little endian takes an int and returns the little endian byte sequence of length"""
+    return n.to_bytes(length, "little")
+
+def little_endian_to_int(b):
+    """ Takes a byte sequence and returns an integer """
+    return int.from_bytes(b, "little")
+
+def decode_base58(s):
+    num = 0
+
+    for c in s:
+        num *= 58
+        num += BASE58_ALPHABET.index(c)
+
+    combined = num.to_bytes(25, byteorder="big")
+    checksum = combined[-4:]
+
+    if hash256(combined[:-4])[:4] != checksum:
+        raise ValueError(f"bad Address {checksum} {hash256(combined[:-4][:4])}")
+
+    return combined[1:-4]
