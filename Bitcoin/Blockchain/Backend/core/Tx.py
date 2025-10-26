5d2a3e244eec2be9a284b5c3f1396a98f805c1d4
Matthew Deng <75967945+mattdengg@users.noreply.github.com>
2025-10-26 15:24:14 -0400

    Created a Transaction Id (#7)

--- Bitcoin/Blockchain/Backend/core/Tx.py
+++ Bitcoin/Blockchain/Backend/core/Tx.py
@@ -1,5 +1,5 @@
 from Blockchain.Backend.core.Script import Script
-from Blockchain.Backend.util.util import int_to_little_endian, bytes_needed, decode_base58, little_endian_to_int
+from Blockchain.Backend.util.util import int_to_little_endian, bytes_needed, decode_base58, little_endian_to_int, encode_varint, hash256
 
 ZERO_HASH = b'\0' * 32
 REWARD = 50
@@ -24,8 +24,9 @@ class CoinbaseTx:
         target_h160 = decode_base58(MINER_ADDRESS)
         target_script = Script.p2pkh_script(target_h160)
         tx_outs.append(TxOut(amount = target_amount, script_pubkey= target_script))
-
-        return Tx(1, tx_ins, tx_outs, 0)
+        coinBaseTx = Tx(1, tx_ins, tx_outs, 0)
+        coinBaseTx.TxId = coinBaseTx.id()
+        return coinBaseTx
 
 
 class Tx:
@@ -34,6 +35,34 @@ class Tx:
         self.tx_inputs = tx_inputs
         self.tx_outputs = tx_outputs
         self.locktime = locktime
+
+    def id(self):
+        """
+        Human-readable Tx id
+        """
+        return self.hash().hex()
+
+    def hash(self):
+        """
+        Binary hash of serialization
+        """
+        return hash256(self.serialize())[::-1]
+
+    def serialize(self):
+        result = int_to_little_endian(self.version, 4)
+        result += encode_varint(len(self.tx_inputs))
+
+        for tx_in in self.tx_inputs:
+            result += tx_in.serialize()
+
+        result += encode_varint(len(self.tx_outputs))
+
+        for tx_out in self.tx_outputs:
+            result += tx_out.serialize()
+
+        result += int_to_little_endian(self.locktime, 4)
+        return result
+
 
     def is_coinbase(self):
         """
@@ -93,7 +122,19 @@ class TxIn:
 
         self.seq = seq
 
+    def serialize(self):
+        result = self.prev_tx[::-1]
+        result += int_to_little_endian(self.prev_index, 4)
+        result += self.script_sig.serialize()
+        result += int_to_little_endian(self.seq, 4)
+        return result
+
 class TxOut:
     def __init__(self, amount, script_pubkey):
         self.amount = amount                # how much we are sending
         self.script_pubkey = script_pubkey  # to who we are sending
+
+    def serialize(self):
+        result = int_to_little_endian(self.amount, 8)
+        result += self.script_pubkey.serialize()
+        return result
--- Bitcoin/Blockchain/Backend/util/util.py
+++ Bitcoin/Blockchain/Backend/util/util.py
@@ -42,3 +42,16 @@ def decode_base58(s):
         raise ValueError(f"bad Address {checksum} {hash256(combined[:-4][:4])}")
 
     return combined[1:-4]
+
+def encode_varint(i):
+    """Encodes an integer as a varint"""
+    if  i < 0xfd:
+        return bytes([i])
+    elif i < 0x10000:
+        return b'\xfd' + int_to_little_endian(i, 2)
+    elif i < 0x100000000:
+        return b'\xfe' + int_to_little_endian(i, 4)
+    elif i < 0x10000000000000000:
+        return b'\xff' + int_to_little_endian(i, 8)
+    else:
+        raise ValueError('Integer too large: {}'.format(i))
